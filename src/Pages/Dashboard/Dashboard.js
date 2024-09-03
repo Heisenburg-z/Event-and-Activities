d57fc5587bf51cbd1f8e86ab3dd6a5bf0be76604
Clement Jele <clemyjele@gmail.com>
2024-09-03 14:16:46 +0200

    mmhh

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import Sidebar from '../../components/Sidebar';
 import { Routes, Route } from 'react-router-dom';
 import EventCalendar from '../../components/EventCalendar';
